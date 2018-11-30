d7a166730cb9bced3dc089c5fd52d51f4bfe97e5
naoya.abe <micropig3402@gmail.com>
2018-11-30 07:11:14 +0000

    set preparation

---
+++ config/initializers/generators.rb
@@ -0,0 +1,9 @@
+# これで、CSS, JS, Helper 関係のファイルは自動生成されません。
+# また、Router にルーティングを自動的に追加することもありません。
+
+Rails.application.config.generators do |g|
+  g.stylesheets false
+  g.javascripts false
+  g.helper false
+  g.skip_routes true
+end
